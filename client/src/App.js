649d1ace8f702f63cbc44e1a507f6b3c262b7d05
Dylan Coffey <dcoffey20@gmail.com>
2020-03-28 11:13:19 -0500

    Revert "Merge branch 'master' into createSql"
    This reverts commit 13287e4009e7da7d9700216f3d93540f787a00c9.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import SignIn from "./components/SignIn/SignIn";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <SignIn />
-      </div>
+      
     </div>
   );
 }
