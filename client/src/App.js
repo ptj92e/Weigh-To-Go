f446530af46ad9aaaf82cfc81101c1407d9574a3
Phillip Jones <ptj92e@gmail.com>
2020-04-02 20:03:04 -0500

    Committing to Merge

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import NewUser from "./components/NewUser/NewUser";
 import Home from "./components/Home/Home";
 import Meals from "./components/Meal/Meal";
 import Workouts from "./components/Workout/Workout";
+import Account from "./components/Account/Account";
 
 function App() {
   return (
@@ -13,10 +14,12 @@ function App() {
       <Router>
         <div>
           <Route exact path="/" component={SignIn} />
-          <Route exact path="/newUser" component={NewUser} />
+          <Route exact path="/sign_up" component={NewUser} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/workouts" component={Workouts} />
           <Route exact path="/meals" component={Meals} />
+          <Route exact path="/account" component={Account} />
+          <Route exact path="/new_user" component={Home} />
         </div>
       </Router>
     </div>
