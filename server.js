6963fbf7d1f90409a364d62dcd47abb2b399403f
Dylan Coffey <dcoffey20@gmail.com>
2020-04-02 20:22:42 -0500

    created sequelize connections

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const mysql = require("mysql");
 const routes = require("./routes");
+const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,27 +14,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-// app.use(routes);
 
-// Connect to the Mongo DB
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3001,
-    user: "root",
-    password: "Trombone-9",
-    database: "weighToGo_db"
-});
-
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
 
 // Start the API server
-app.listen(PORT, function () {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    });
 });
