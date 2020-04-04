1a122e62ce0ae5d1876e4a56cc3ac1937468e0c2
Phillip Jones <ptj92e@gmail.com>
2020-04-04 12:13:17 -0500

    Set State for NewUserForm

--- client/src/components/SignIn/NewUserForm/NewUserForm.js
+++ client/src/components/SignIn/NewUserForm/NewUserForm.js
@@ -2,20 +2,54 @@ import React, { Component } from "react";
 import NewUserButton from "../../NewUser/NewUserButton/NewUserButton";
 import "./NewUserForm.css";
 
-class NewUserForm extends Component { 
+class NewUserForm extends Component {
+    state = {
+        name: "",
+        email: "",
+        newPass: "",
+        confirmPass: ""
+    };
+
+    handleChange = e => {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
     render() {
         return (
             <div>
                 <h3>If you are a new user, sign up here:</h3>
                 <form>
                     <label>Name</label>
-                    <input type="text" id="newName"></input>
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
                     <label>E-Mail</label>
-                    <input type="text" id="newEmail"></input>
+                    <input
+                        type="text"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
                     <label>Password</label>
-                    <input type="text" id="newPassword"></input>
+                    <input
+                        type="password"
+                        name="newPass"
+                        value={this.state.newPass}
+                        onChange={this.handleChange}
+                    />
                     <label>Confirm Password</label>
-                    <input type="text" id="confirmPassword"></input>
+                    <input
+                        type="password"
+                        name="confirmPass"
+                        value={this.state.confirmPass} onChange={this.handleChange}
+                    />
                     <NewUserButton />
                 </form>
             </div>
