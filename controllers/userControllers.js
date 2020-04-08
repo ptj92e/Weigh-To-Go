fccb622c0de88d7c1587147dce4c0adea5352eee
Phillip Jones <ptj92e@gmail.com>
2020-04-07 20:17:08 -0500

    It works

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,9 @@
-db = require("../models/user");
+db = require("../models");
 
 module.exports = {
     newUser: function(req, res) {
         console.log(req.body);
-        db.User.create(req.body).then(function(dbUser) {
-            res.json(dbUser);
-        });
+        db.User.create(req.body)
+            .then(dbUser => res.json(dbUser));
     }
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
 
-router.use("/api/user", userRoutes);
+router.use("/", userRoutes);
 
 module.exports = router;
