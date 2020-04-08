054933b1bcb11cd262e71e126f8b9ac85cfee4b1
Phillip Jones <ptj92e@gmail.com>
2020-04-07 21:24:14 -0500

    Please work

--- config/config.js
+++ config/config.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 module.exports = {
-"development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_KEY,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-},
-"test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-},
-"production": {
-    "use_env_variable": process.env.JAWSDB_URL, 
-    "dialect": "mysql",
-    "operatorsAliases": false,
-  }
+    "development": {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_KEY,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
 };
