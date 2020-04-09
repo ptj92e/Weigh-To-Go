4afab892630cc29445ee92f38effa42d965a1ad3
Phillip Jones <ptj92e@gmail.com>
2020-04-09 17:18:56 -0500

    Started Styling NavBar

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,17 +1,10 @@
 import React from "react";
-import SignInForm from "../components/SignInForm/SignInForm";
 import SignInTitle from "../components/SignInTitle/SignInTitle";
-import NewUserForm from "../components/NewUserForm/NewUserForm";
-import "./SignIn.css";
 
 function SignIn() {
     return (
         <div>
             <SignInTitle />
-            <div id="formsDiv">
-                <SignInForm />
-                <NewUserForm />
-            </div>
         </div>
     );
 };
