140ae2198c46a6f28e7e62a535401bbca3591357
Phillip Jones <ptj92e@gmail.com>
2020-04-16 21:21:42 -0500

    Committing for the night

--- client/src/pages/Meal.js
+++ client/src/pages/Meal.js
@@ -4,9 +4,20 @@ import API from "../utils/API";
 
 function Meals() {
     const searchRef = useRef();
+    const nameRef = useRef();
+    const servingRef = useRef();
     const [searchState, setSearchState] = useState([]);
     const [resultState, setResultState] = useState({});
     const [recipeState, setRecipeState] = useState([]);
+    const [mealState, setMealState] = useState({
+        name: "",
+        calories: "",
+        servings: "",
+        total_calories: "",
+        fat: "",
+        carbs: "",
+        protein: ""
+    });
 
     useEffect(() => {
 
@@ -27,40 +38,89 @@ function Meals() {
 
     const addFood = e => {
         e.preventDefault();
-        recipeState.concat(
-            {
-                calories: resultState.calories,
-                fat: resultState.fat,
-                carbs: resultState.carbs,
-                protein: resultState.protein
-            }
+        setRecipeState(
+            [
+                ...recipeState,
+                {
+                    calories: resultState.calories,
+                    fat: resultState.fat,
+                    carbs: resultState.carbs,
+                    protein: resultState.protein
+                }
+            ]
         );
-        console.log(recipeState);
+    }
+
+    const nutritionCalc = e => {
+        e.preventDefault();
+        let name = nameRef.current.value;
+        let servings = servingRef.current.value;
+        let calories = 0;
+        let fat = 0;
+        let carbs = 0;
+        let protein = 0;
+        if (recipeState.length === 0) {
+            alert("There are no ingredients");
+        } else {
+            for (let i = 0; i < recipeState.length; i++) {
+                calories = calories += recipeState[i].calories;
+                fat = fat += recipeState[i].fat;
+                carbs = carbs += recipeState[i].carbs;
+                protein = protein += recipeState[i].protein;
+            }
+            setMealState({
+                name: name,
+                servings: servings,
+                calories: calories,
+                total_calories: calories,
+                fat: fat,
+                carbs: carbs,
+                protein: protein
+            });
+        }
     }
 
     return (
         <div>
             <Navbar />
-            <h1>What are you eating today?</h1>
-            <form onSubmit={searchFood}>
-                <h4>Search for a food:</h4>
+            <h1>What is the name of this meal?</h1>
+            <form>
                 <input
                     type="text"
-                    ref={searchRef}
-                    onChange={() => {
-                        setSearchState({
-                            search: searchRef.current.value
-                        })
-                    }}
+                    required ref={nameRef}
                 />
-                <button type="submit">Search</button>
+                <p>How many servings did you have?</p>
+                <select ref={servingRef}>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                </select>
+                <form onSubmit={searchFood}>
+                    <h4>Search for a food:</h4>
+                    <input
+                        type="text"
+                        ref={searchRef}
+                        onChange={() => {
+                            setSearchState({
+                                search: searchRef.current.value
+                            })
+                        }}
+                    />
+                    <button type="submit">Search</button>
+                    <ul>
+                        {resultState.name ?
+                            <li>{resultState.name}<button onClick={addFood}>Add Food</button></li> :
+                            <li>Search Results</li>}
+                    </ul>
+                </form>
+                <button onClick={nutritionCalc} type="submit">Save Meal</button>
             </form>
-            <button onClick={() => { console.log(resultState) }}>Please Work</button>
-            <ul>
-                {resultState.name ?
-                    <li>{resultState.name}<button onClick={addFood}>Add Food</button></li> :
-                    <li>Search Results</li>}
-            </ul>
+            <button onClick={() => console.log(mealState)}>Please Have Worked</button>
         </div>
     )
 }
